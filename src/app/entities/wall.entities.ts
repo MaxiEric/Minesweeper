4788e18f9c04caadcaf6d3bd76383abf0df587ac
MaxiEric <maxiericgonzalez@gmail.com>
2019-11-20 22:07:02 -0300

    set random mine in block

--- src/app/entities/wall.entities.ts
+++ src/app/entities/wall.entities.ts
@@ -1,4 +1,12 @@
 import { Block } from './block.entities';
+const fellowBlock = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
+
+/**
+ * Wall class formed by a block matrix
+ * @param block actual state
+ * @param remainingBlocks  remaining blocks
+ * @param mineCount mine counter
+ */
 
 export class Wall {
     public block: Block[][] = [];
@@ -14,5 +22,11 @@ export class Wall {
             }
         }
         console.log('Block structure to form the wall', this.block);
+        for (let i = 0; i < mines; i++) {
+            const y = Math.floor(Math.random() * this.block.length);
+            const x = Math.floor(Math.random() * this.block[y].length);
+            this.block[y][x].mine = true;
+        }
+        console.log('mineCount: ', this.mineCount);
     }
 }
