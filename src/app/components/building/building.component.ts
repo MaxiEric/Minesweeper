6f164e88c843094da984164672555177523005c5
MaxiEric <maxiericgonzalez@gmail.com>
2019-11-21 01:15:22 -0300

    add time

--- src/app/components/building/building.component.ts
+++ src/app/components/building/building.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Wall } from 'src/app/entities/wall.entities';
 import { Block } from 'src/app/entities/block.entities';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-building',
@@ -9,15 +10,33 @@ import { Block } from 'src/app/entities/block.entities';
 })
 export class BuildingComponent {
 
-  wall: Wall;
+  public wall: Wall;
+  private intervalTime: intervalTime;
+  public time = 0;
 
   constructor() {
     this.wall = new Wall(10, 5);
+    const source = timer(1000, 2000);
+  }
+
+  // TODO Find another alternative to change this method
+  private startTheGame() {
+    this.interval = setInterval(() => {
+      this.time ++;
+    },1000)
   }
 
   public checkBlock( block: Block) {
+    this.time === 0 ? this.startTheGame() : '';
     const resultCheckBlock = this.wall.checkBlock(block);
-    resultCheckBlock === 'gameover' ? alert('You lose') : resultCheckBlock === 'win' ? alert('you win'):'';
+    
+    if( resultCheckBlock === 'gameover' ){
+      clearInterval(this.interval);
+      alert('Perdiste!')
+    }else if(resultCheckBlock === 'win'){
+      clearInterval(this.interval);
+      alert('Ganaste')
+    }
   }
 
   public flag(block: Block){
@@ -25,6 +44,8 @@ export class BuildingComponent {
   }
 
   public startAgain(){
+    this.time = 0;
+    this.startTheGame(); ;
     this.wall = new Wall(10, 5);
   }
 }
